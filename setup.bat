cfb52e16d0ff377d1905d0c5af4008cd29a31940
Collin Rodes <steelproxy@protonmail.com>
2024-11-21 21:54:34 -0500

    added condition to run twitta if already setup

--- setup.bat
+++ setup.bat
@@ -1,18 +1,21 @@
 @echo off
-echo Creating virtual environment...
-python -m venv .venv
-
-echo Activating virtual environment...
-call .venv\Scripts\activate
-
-echo Installing requirements...
-pip install -r requirements.txt
+if not exist ".venv" (
+    echo Creating virtual environment...
+    python -m venv .venv
+    
+    echo Installing requirements...
+    call .venv\Scripts\activate
+    pip install -r requirements.txt
+) else (
+    echo Virtual environment already exists, activating...
+    call .venv\Scripts\activate
+)
 
 echo Starting Twitta...
-python twitta.py
+python src/twitta.py
 
 echo.
 echo If you want to run Twitta again later, just use:
 echo call .venv\Scripts\activate
-echo python twitta.py
+echo python src/twitta.py
 pause
--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "Creating virtual environment..."
-python3 -m venv .venv
-
-echo "Activating virtual environment..."
-source .venv/bin/activate
-
-echo "Installing requirements..."
-pip install -r requirements.txt
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3.12 -m venv .venv
+    
+    echo "Activating virtual environment..."
+    source .venv/bin/activate
+    
+    echo "Installing requirements..."
+    pip install -r requirements.txt
+else
+    echo "Virtual environment already exists, activating..."
+    source .venv/bin/activate
+fi
 
 echo "Making script executable..."
-chmod +x ./twitta.py
+chmod +x src/twitta.py
 
 echo "Starting Twitta..."
-python3 twitta.py
+python src/twitta.py
 
 echo ""
 echo "If you want to run Twitta again later, just use:"
